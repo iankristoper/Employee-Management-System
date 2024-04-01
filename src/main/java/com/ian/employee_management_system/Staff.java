899f47458aa0855989678bb19d7fbbe926dc2d94
iankristoper <iankristoper26@gmail.com>
2024-04-01 10:22:42 +0800

    UPDATE: sub class features

---
+++ src/main/java/com/ian/employee_management_system/Staff.java
@@ -0,0 +1,25 @@
+package com.ian.employee_management_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Staff extends Employee
+{
+    //constructor to initialize the name and salary of staff
+    public Staff(String name, double salary)
+    {
+        super(name, salary);
+    }
+    
+    
+    // Implementation of performDuties() method for Staff role
+    @Override
+    public void performDuties() 
+    {
+        System.out.println("Staff " + getName() + " is performing general staff duties.");
+    }
+    
+    
+}
