63606de4251a85933aaad94fa5102a9ca13aca39
iankristoper <iankristoper26@gmail.com>
2024-04-01 10:31:00 +0800

    UPDATE: sub class features

--- src/main/java/com/ian/employee_management_system/Contractor.java
+++ src/main/java/com/ian/employee_management_system/Contractor.java
@@ -5,7 +5,22 @@ package com.ian.employee_management_system;
  * @author Ian Tagano
  */
 
-public class Contractor 
+public class Contractor extends Employee
 {
+    //private intances variable for constrctor 
+    private String project;
     
+    // Constructor to initialize name, salary, and project
+    public Contractor(String name, double salary, String project) 
+    {
+        super(name, salary);
+        this.project = project;
+    }
+    
+    // Implementation of performDuties() method for Contractor role
+    @Override
+    public void performDuties() 
+    {
+        System.out.println("Contractor " + getName() + " is working on project: " + project);
+    }
 }
