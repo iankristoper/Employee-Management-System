586362da2afb62de03c51ec80c224f9b1b59fa1e
iankristoper <iankristoper26@gmail.com>
2024-04-01 10:22:33 +0800

    UPDATE: sub class features

---
+++ src/main/java/com/ian/employee_management_system/Manager.java
@@ -0,0 +1,26 @@
+package com.ian.employee_management_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Manager extends Employee
+{
+    //additional instance variable for manager
+    private String department;  
+    
+    //constructor to initialize name, salary, and department
+    public Manager(String name, double salary, String department)
+    {
+        super(name, salary);
+        this.department = department;
+    }
+    
+    //implementation of perform duties using method overriding 
+    @Override
+    public void performDuties()
+    {
+        System.out.println("Manager " + getName() + " is managing the " + department + " department.");
+    }
+}
