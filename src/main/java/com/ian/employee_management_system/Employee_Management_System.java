534ffdc2e941e23ee726986bff6001f703171bfb
iankristoper <iankristoper26@gmail.com>
2024-04-01 10:22:11 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/employee_management_system/Employee_Management_System.java
@@ -0,0 +1,15 @@
+package com.ian.employee_management_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Employee_Management_System 
+{
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
