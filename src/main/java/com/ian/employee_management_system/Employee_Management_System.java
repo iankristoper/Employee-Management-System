ae56494e90e135a44b215bfa3ffe22522a5a88b7
iankristoper <iankristoper26@gmail.com>
2024-04-01 10:30:49 +0800

    UPDATE: main class features

--- src/main/java/com/ian/employee_management_system/Employee_Management_System.java
+++ src/main/java/com/ian/employee_management_system/Employee_Management_System.java
@@ -10,6 +10,17 @@ public class Employee_Management_System
 
     public static void main(String[] args) 
     {
+        Employee manager = new Manager("Ian", 500000, "Ian Company");
+        Employee staff = new Staff("John Doe", 30000);
+        Employee contractor = new Contractor("Elon musk", 59000, "Devin AI");
         
+        
+        System.out.println("Ian Company - Employee duties");
+        manager.performDuties();
+        staff.performDuties();
+        contractor.performDuties();
+        
+        
+                
     }
 }
